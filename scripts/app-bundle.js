62d7b260bfca5ab2ad29311c3640e4a332420a11
nsrchemie <nsrchemie@gmail.com>
2017-03-06 18:20:39 -0500

    working on updating FE dynamically with input return

---
+++ scripts/app-bundle.js
@@ -0,0 +1,87 @@
+define('app',['exports'], function (exports) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  function _classCallCheck(instance, Constructor) {
+    if (!(instance instanceof Constructor)) {
+      throw new TypeError("Cannot call a class as a function");
+    }
+  }
+
+  var App = exports.App = function () {
+    function App() {
+      _classCallCheck(this, App);
+
+      this.message = 'Hello World!';
+    }
+
+    App.prototype.greeting = function greeting() {
+      alert('Hi ${this.name}');
+    };
+
+    return App;
+  }();
+});
+define('environment',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.default = {
+    debug: true,
+    testing: true
+  };
+});
+define('main',['exports', './environment'], function (exports, _environment) {
+  'use strict';
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+
+  var _environment2 = _interopRequireDefault(_environment);
+
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
+
+  Promise.config({
+    warnings: {
+      wForgottenReturn: false
+    }
+  });
+
+  function configure(aurelia) {
+    aurelia.use.standardConfiguration().feature('resources');
+
+    if (_environment2.default.debug) {
+      aurelia.use.developmentLogging();
+    }
+
+    if (_environment2.default.testing) {
+      aurelia.use.plugin('aurelia-testing');
+    }
+
+    aurelia.start().then(function () {
+      return aurelia.setRoot();
+    });
+  }
+});
+define('resources/index',["exports"], function (exports) {
+  "use strict";
+
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+  exports.configure = configure;
+  function configure(config) {}
+});
+define('text!app.html', ['module'], function(module) { module.exports = "<template><h2>${message}</h2><input value.bind=\"name\"> <button click.delegate=\"greeting()\">Greet!</button></template>"; });
+//# sourceMappingURL=app-bundle.js.map
--- src/app.js
+++ src/app.js
@@ -2,4 +2,8 @@ export class App {
   constructor() {
     this.message = 'Hello World!';
   }
+
+ greeting() {
+   alert('Hi ${this.name}');
+ }
 }
